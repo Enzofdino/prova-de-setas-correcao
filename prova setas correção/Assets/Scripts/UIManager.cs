95396e599132903ab7f62f8a8992f269cdd82d60
Enzofdino <enzofhostin@gmail.com>
2024-07-10 09:09:20 -0300

    atualizar setas
    metodo atualizar setas

--- prova setas correção/Assets/Scripts/UIManager.cs
+++ prova setas correção/Assets/Scripts/UIManager.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static UIManager Instance;
+
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] Image[] imagens;
+    [SerializeField] TextMeshProUGUI textoDePontuacao, textodorelogio;
+    private void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void AtualizarSetas(KeyCode[] setas)
     {
-        
+       for(int i = 0; i < setas.Length; i++)
+        {
+            if (i >= setas.Length)
+            {
+                imagens[i].sprite = sprites[0];
+            }
+            else if (setas[i] == KeyCode.DownArrow)
+            {
+                imagens[i].sprite = sprites[1];
+            }
+            else if (setas[i] == KeyCode.UpArrow)
+            {
+                imagens[i].sprite = sprites[2];
+            }
+            else if (setas[i] == KeyCode.LeftArrow)
+            {
+                imagens[i].sprite = sprites[3];
+            }
+            else if (setas[i] == KeyCode.RightArrow)
+            {
+                imagens[i].sprite = sprites[4];
+            }
+
+            imagens[i].color = Color.white;
+        }
     }
+    
+    void AtualizarSetas(int setaSelecionada,bool acertou)
+    {
+        if (acertou)
+        {
+            imagens[setaSelecionada].color = Color.green;
+        }
+        else
+        {
+            imagens[setaSelecionada].color = Color.red;
+        }
+    }
+
 }

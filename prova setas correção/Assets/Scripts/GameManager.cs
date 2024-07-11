be3845faadaa8e973ca1c225502b874eb4bfdb57
Enzofdino <enzofhostin@gmail.com>
2024-07-11 11:37:17 -0300

    todos os comentarios de GamaManager

--- prova setas correção/Assets/Scripts/GameManager.cs
+++ prova setas correção/Assets/Scripts/GameManager.cs
@@ -4,80 +4,84 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    int pontos, teclaAtual;
-    float relogio;
-    KeyCode[] teclas;
+    int pontos, teclaAtual; //variaveis inteiras chamadas pontos e teclaatual
+    float relogio; //variavel float chamada relogio
+    KeyCode[] teclas; //array keycode[] chamada teclas
 
-    private void Start()
+    private void Start() //metodo privado start 
     {
-        GerarSetas();
+        GerarSetas();//chama o metodo gerarsetas
     }
-    private void Update()
+    private void Update() //metodo privado update 
     {
-        if (Input.GetKeyUp(KeyCode.DownArrow))
+        if (Input.GetKeyUp(KeyCode.DownArrow)) //se o input.getkeyup(Teclapressinada) for o down arrow de keycode
         {
-            ChecarTecla(KeyCode.DownArrow);
+            ChecarTecla(KeyCode.DownArrow); //chama o metodo checartecla para parametro sendo array keycode downarrow
         }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))//se o input.getkeyup(Teclapressinada) for o left arrow de keycode
         {
-            ChecarTecla(KeyCode.LeftArrow);
+            ChecarTecla(KeyCode.LeftArrow);//chama o metodo checartecla para parametro sendo array keycode leftarrow
 
         }
-        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        else if (Input.GetKeyUp(KeyCode.UpArrow))//se o input.getkeyup(Teclapressinada) for o up arrow de keycode
         {
-            ChecarTecla(KeyCode.UpArrow);
+            ChecarTecla(KeyCode.UpArrow);//chama o metodo checartecla para parametro sendo array keycode uparrow
 
         }
-        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        else if (Input.GetKeyUp(KeyCode.RightArrow))//se o input.getkeyup(Teclapressinada) for o right arrow de keycode
         {
-            ChecarTecla(KeyCode.RightArrow);
+            ChecarTecla(KeyCode.RightArrow);//chama o metodo checartecla para parametro sendo array keycoderightarrow
 
         }
-        ContagemRegressiva();
-    }
-
-    void ContagemRegressiva()
-    {
-        relogio -= Time.deltaTime;
-
-        //UIManagaer.instance.AtualizarTextos(pontos,relogio);
-
-        if (relogio <= 0)
-        {
-            pontos -= teclas.Length - teclaAtual;
-            GerarSetas();
-        }
-    }
-    void GerarSetas()
-    {
-        teclaAtual = 0;
-        teclas = new KeyCode[Random.Range(5, 15)];
-        for (int i = 0; i < teclas.Length; i++)
-        {
-            teclas[i] = (KeyCode)Random.Range(273, 276);
-        }
-        relogio = teclas.Length / 2;
-        //UIManager.instance.AtualizarSetas(teclas);
+        ContagemRegressiva(); //chama o metodo contagemregressiva dentro ainda do metodo update
     }
 
-    void ChecarTecla(KeyCode teclaPressionada)
+    void ContagemRegressiva() //metodo contagemregressiva
     {
-        if (teclaPressionada == teclas[teclaAtual])
-        {
-            pontos++;
-            //UIManager.instance.AtualizarSeta(teclaAtual,true);
-        }
-        else
-        {
-            pontos--;
-            relogio--;
-            //UIManager.instance.AtualizarSeta(teclaAtual,false);
-        }
-        //UIManager.instance.AtualizarSeta(pontos,relogio);
-        teclaAtual++;
-        if (teclaAtual == 0)
-        {
+        relogio -= Time.deltaTime; // diminui o igual do relogio por time.deltatime
 
+        //UIManagaer.instance.AtualizarTextos(pontos,relogio); //atualizando os textos pontos e relogio de UIManager
+
+        if (relogio <= 0) //se relogio menor ou igual a 0
+        {
+            pontos -= teclas.Length - teclaAtual; //diminui a diferença do comprimento das teclas para teclaatual de pontos
+            GerarSetas(); // chama o metodo gerarsetas
+        }
+    }
+    void GerarSetas() //metodo gerarsetas
+    {
+        teclaAtual = 0; //teclaatual = 0
+        teclas = new KeyCode[Random.Range(5, 15)]; //teclas inicia teclas com valores random.range aleatorios entre 5 e 15
+        for (int i = 0; i < teclas.Length; i++) //loop para preencher o array de teclas 
+        {
+            teclas[i] = (KeyCode)Random.Range(273, 276); // preenche o array de teclas com valores com random.range aleatorios entre 273 e 276
+            //codigo para as setas direcionadas
+        }
+        relogio = teclas.Length / 2; //define o relogio com a metade do valor do comprimento de teclas dividido por 2 
+        //UIManager.instance.AtualizarSetas(teclas); //atualiza as teclas do metodo atualizarsetas da instancia de uimanager
+    }
+
+    void ChecarTecla(KeyCode teclaPressionada) //metodo chamado checartecla com parametro array de keycode chamado teclapressionada
+    {
+        if (teclaPressionada == teclas[teclaAtual]) //se a teclapressionada for igual a array de teclas para parametro teclaatual
+        {
+            pontos++; //aumenta (incrementa) pontos
+            //UIManager.instance.AtualizarSeta(teclaAtual,true); //atualiza a teclaatual como true do metodo atualizarseta
+            //da instancia do UIManager
+        }
+        else //senao
+        {
+            pontos--; //diminui ou decrementa os pontos
+            relogio--;//diminui ou decrementa o relogio
+            //UIManager.instance.AtualizarSeta(teclaAtual,false); //atualiza a teclaatual como false do metodo atualizarseta
+            //da instancia do UIManager
+        }
+        //UIManager.instance.AtualizarSeta(pontos,relogio); //atualiza o parametro pontos e relogio do metodo atualizarseta
+        //da instancia de uimanager
+        teclaAtual++;//aumenta a teclaatual
+        if (teclaAtual == 0) //se a teclaatual for igual ao comprimento do array de teclas
+        {
+            GerarSetas (); //chama o metodo gerarsetas
         }
     }
 }
